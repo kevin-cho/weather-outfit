9f4fc06f250395737e10f76b1045783d2f766055
Kevin Cho <chokevinj@gmail.com>
2020-07-10 15:10:16 -0400

    Import react-bootstrap, react-router-dom, and axios

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Home = () => (
+  <div>
+    Home
+    <Button variant="primary">Primary</Button>
+  </div>
+);
+
+export default Home;
