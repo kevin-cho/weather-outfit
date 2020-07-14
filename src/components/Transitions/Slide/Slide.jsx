a9a0b8fcbe8afbc44d67545c0330aed926a75142
Kevin Cho <chokevinj@gmail.com>
2020-07-14 15:58:40 -0400

    Add up down options to Slide

--- src/components/Transitions/Slide/Slide.jsx
+++ src/components/Transitions/Slide/Slide.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { CSSTransition } from 'react-transition-group';
 import './Slide.css';
 
-const Slide = ({ direction = 'left', ...props }) => (
+const propTypes = {
+  direction: PropTypes.oneOf(['left', 'right', 'up', 'down'])
+};
+
+const Slide = ({ direction = 'left', children, ...props }) => (
   <CSSTransition
     classNames={`slide-${direction}`}
     timeout={500}
@@ -10,10 +15,10 @@ const Slide = ({ direction = 'left', ...props }) => (
     {...props}
   >
     {/* Extra <div> allows transition to work on multiple children nodes */}
-    <div>
-      {props.children}
-    </div>
+    <div>{children}</div>
   </CSSTransition>
 );
+
+Slide.propTypes = propTypes;
 
 export default Slide;
